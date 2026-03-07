13174d354b3d7080a99f6c8401050a5050eaf551
DreamAi <heuerwayne@gmail.com>
2026-03-07 13:26:28 +0200

    Create Operations Center.js

---
+++ frontend/src/pages/Operations Center.js
@@ -0,0 +1,21 @@
+import React from "react"
+import RevenueGraph from "../components/RevenueGraph"
+import SystemHealthPanel from "../components/SystemHealthPanel"
+
+export default function OperationsCenter(){
+
+return(
+
+<div>
+
+<h1>Raging Boards Operations Center</h1>
+
+<RevenueGraph/>
+
+<SystemHealthPanel/>
+
+</div>
+
+)
+
+}
