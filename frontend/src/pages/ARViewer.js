8c667c860589b4b6b1bbe26a8b97674517719d98
DreamAi <heuerwayne@gmail.com>
2026-03-07 13:31:04 +0200

    Create ARViewer.js

---
+++ frontend/src/pages/ARViewer.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function ARViewer(){
+
+return(
+
+<div>
+
+<h2>AR Surfboard Preview</h2>
+
+<p>View your board in augmented reality on supported devices.</p>
+
+<a href="/ar/board-viewer">Launch AR</a>
+
+</div>
+
+)
+
+}
