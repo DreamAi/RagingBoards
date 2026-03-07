43f2700cdac5418bdc821b12168d2b938a31b66f
DreamAi <heuerwayne@gmail.com>
2026-03-07 14:46:28 +0200

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Store from "./pages/Store"
+import Cart from "./components/Cart"
+
+export default function App(){
+
+return(
+
+<div>
+
+<Store/>
+<Cart/>
+
+</div>
+
+)
+
+}
