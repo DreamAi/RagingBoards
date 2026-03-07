ddc434e0f7197964832284df7fe5b6557f47bcd8
DreamAi <heuerwayne@gmail.com>
2026-03-07 15:17:29 +0200

    Create seo_engine.py

---
+++ marketing/seo_engine.py
@@ -0,0 +1,14 @@
+keywords = [
+
+"surfboard design software",
+"surfboard graphics creator",
+"AI surfboard design",
+"surfboard shaper tools"
+
+]
+
+def add_seo(text):
+
+    tags = " ".join(["#"+k.replace(" ","") for k in keywords])
+
+    return text + " " + tags
