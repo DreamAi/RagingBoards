58b6e1baef11a781a36a6400282685230b580563
DreamAi <heuerwayne@gmail.com>
2026-03-07 14:38:40 +0200

    Create marketing_activity.py

---
+++ operations_center/marketing_activity.py
@@ -0,0 +1,16 @@
+campaigns = [
+
+"Instagram promotion",
+"LinkedIn article",
+"TikTok demo video"
+
+]
+
+def marketing_stats():
+
+    return {
+
+        "active_campaigns": campaigns,
+        "campaign_count": len(campaigns)
+
+    }
