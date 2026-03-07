1cac7bf83612f882df4ffba4c86812b61021d383
DreamAi <heuerwayne@gmail.com>
2026-03-07 14:34:44 +0200

    Create design_engine.py

---
+++ design_studio/design_engine.py
@@ -0,0 +1,15 @@
+designs = []
+
+def create_design(board, colors, pinlines):
+
+    design = {
+
+        "board": board,
+        "colors": colors,
+        "pinlines": pinlines
+
+    }
+
+    designs.append(design)
+
+    return design
