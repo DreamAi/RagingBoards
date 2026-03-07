28ec4d7edce18c1b90f1cc8b2f8f05a39bd4da57
DreamAi <heuerwayne@gmail.com>
2026-03-07 15:16:46 +0200

    Create social_publisher.py

---
+++ marketing/social_publisher.py
@@ -0,0 +1,18 @@
+platforms = [
+
+"Instagram",
+"LinkedIn",
+"Facebook",
+"X",
+"YouTube",
+"TikTok"
+
+]
+
+def publish(post):
+
+    for p in platforms:
+
+        print(f"Publishing to {p}:")
+        print(post)
+        print("")
